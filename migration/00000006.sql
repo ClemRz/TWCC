80c78cd2dbea140136c488a16418b635fb1d715a
cronzon <cronzon@switchfly.com>
2014-04-19 03:04:42 +0000

    Add foreign key constraints and change tables engine - issue #20

--- migration/00000006.sql
+++ migration/00000006.sql
@@ -1,4 +1,4 @@
 ALTER TABLE `countries` CHANGE `Iso` `Iso_countries` VARCHAR( 2 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
 ALTER TABLE `country_coordinate_system` CHANGE `Iso` `Iso_countries` VARCHAR( 2 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
-ALTER TABLE `country_coordinate_system` CHANGE `Id_crs` `Id_coordinate_systems` SMALLINT( 5 ) UNSIGNED NOT NULL;
+ALTER TABLE `country_coordinate_system` CHANGE `Id_crs` `Id_coordinate_systems` SMALLINT( 5 ) NOT NULL;
 ALTER TABLE `coordinate_systems` CHANGE `Id` `Id_coordinate_systems` SMALLINT( 5 ) NOT NULL AUTO_INCREMENT;
