f33d3d79d58a2fd7fed3a49322962e7dffdf08aa
cronzon <cronzon@switchfly.com>
2017-10-19 19:46:44 +0000

    Fixes facebook iframes

--- webapp/templates/index.php
+++ webapp/templates/index.php
@@ -202,7 +202,7 @@ echo(getAlternateReferences());
 								</li>
 								<li class="nav_li">
 <?php if (USE_FACEBOOK && false) { ?>
-									&nbsp;<iframe src="https://www.facebook.com/plugins/like.php?locale=<?php echo LOCALE; ?>&amp;href=http%3A%2F%2Fwww.facebook.com%2FTWCC.free&amp;layout=standard&amp;show_faces=false&amp;action=like&amp;font=arial&amp;colorscheme=light&amp;width=250&amp;height=66" style="border:none; overflow:hidden; width:250px; height:66px;position:absolute;"></iframe>
+									&nbsp;<iframe src="//www.facebook.com/plugins/like.php?locale=<?php echo LOCALE; ?>&amp;href=http%3A%2F%2Fwww.facebook.com%2FTWCC.free&amp;layout=standard&amp;show_faces=false&amp;action=like&amp;font=arial&amp;colorscheme=light&amp;width=250&amp;height=66" style="border:none; overflow:hidden; width:250px; height:66px;position:absolute;"></iframe>
 <?php } ?>
 								</li>
 							</ul>
@@ -444,7 +444,7 @@ echo($html);
 		<?php echo ABOUT_CONTENT; ?>
 				<div><p><img src="<?php echo DIR_WS_IMAGES; ?>star.png" alt="" width="16" height="16"> <a class="link show-p-poll" href=""><?php echo POLL; ?></a></p></div>
 <?php if (USE_FACEBOOK) { ?>
-				<div><iframe src="https://www.facebook.com/plugins/likebox.php?locale=<?php echo LOCALE; ?>&amp;href=http%3A%2F%2Fwww.facebook.com%2FTWCC.free&amp;width=292&amp;colorscheme=light&amp;font=arial&amp;show_faces=false&amp;stream=false&amp;header=false&amp;height=66" style="border:none; overflow:hidden; width:100%; height:66px;"></iframe></div>
+				<div><iframe src="//www.facebook.com/plugins/likebox.php?locale=<?php echo LOCALE; ?>&amp;href=http%3A%2F%2Fwww.facebook.com%2FTWCC.free&amp;width=292&amp;colorscheme=light&amp;font=arial&amp;show_faces=false&amp;stream=false&amp;header=false&amp;height=66" style="border:none; overflow:hidden; width:100%; height:66px;"></iframe></div>
 <?php } ?>
 				<a href="#" title="<?php echo CONTACT_US; ?>" class="contact contact-btn"><?php echo CONTACT_US; ?></a>
 				<div id="app-versions"></div>
