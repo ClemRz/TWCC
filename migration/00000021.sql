0205171cc105bb8bdcbb9cf3136de335f87c876a
cronzon <cronzon@switchfly.com>
2018-09-26 16:27:40 +0000

    Adds a new CRS

--- migration/00000021.sql
+++ migration/00000021.sql
@@ -89,3 +89,9 @@ INSERT INTO `country_coordinate_system` (`Iso_countries`, `Id_coordinate_systems
 ('ZA', 402);
 
 UPDATE `coordinate_systems` SET `Code` = 'EPSG:2055' WHERE `Url` = 'http://spatialreference.org/ref/epsg/2055/';
+
+INSERT INTO `coordinate_systems` (`Locked`, `Id_author`, `Id_reviewer`, `Id_locker`, `Date_inscription`, `Date_reviewed`, `Date_locked`, `Code`, `Definition`, `Bounds`, `Url`, `Enabled`, `Is_connector`) VALUES
+('NO', 1, NULL, NULL, NOW(), NULL, NULL, 'EPSG:22277', '+title=Cape / Lo17 +proj=tmerc +lat_0=0 +lon_0=17 +k=1 +x_0=0 +y_0=0 +axis=swu +a=6378249.145 +b=6356514.966398753 +towgs84=-136,-108,-292,0,0,0,0 +units=m +no_defs', NULL, 'http://spatialreference.org/ref/epsg/22277/', 'YES', 'NO');
+
+INSERT INTO `country_coordinate_system` (`Iso_countries`, `Id_coordinate_systems`) VALUES
+('ZA', 403);
