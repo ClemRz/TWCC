9dc641d2ea1a56ef850487d732b1620589dfcf89
cronzon <cronzon@switchfly.com>
2019-01-17 16:30:04 +0000

    Adds a CRS to the DB

--- migration/00000021.sql
+++ migration/00000021.sql
@@ -101,3 +101,9 @@ INSERT INTO `coordinate_systems` (`Locked`, `Id_author`, `Id_reviewer`, `Id_lock
 
 INSERT INTO `country_coordinate_system` (`Iso_countries`, `Id_coordinate_systems`) VALUES
 ('ZA', 404);
+
+INSERT INTO `coordinate_systems` (`Locked`, `Id_author`, `Id_reviewer`, `Id_locker`, `Date_inscription`, `Date_reviewed`, `Date_locked`, `Code`, `Definition`, `Bounds`, `Url`, `Enabled`, `Is_connector`) VALUES
+('NO', 1, NULL, NULL, NOW(), NULL, NULL, 'EPSG:23090', '+title=ED50 / TM 0 N +proj=tmerc +lat_0=0 +lon_0=0 +k=0.9996 +x_0=500000 +y_0=0 +ellps=intl +units=m +no_defs', NULL, 'http://spatialreference.org/ref/epsg/23090/', 'YES', 'NO');
+
+INSERT INTO `country_coordinate_system` (`Iso_countries`, `Id_coordinate_systems`) VALUES
+('GB', 405);
