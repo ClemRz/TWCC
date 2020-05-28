ffccff376c793e65218887064755bc6e78c9fc96
Clément Ronzon <clem.rz@gmail.com>
2020-05-28 17:27:28 -0500

    Updates web service and manage dependencies with Composer

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ For any questions or suggestions please contact us.
 
 You can donate to support this initiative.
 
-## How to consume the web service: -- CURRENTLY OUT OF SERVICE --
+## How to consume the web service:
 Web service is still on beta version, consume it at your own risks:
 
  - Url of the web-service: http://twcc.fr/en/ws/
@@ -27,18 +27,28 @@ Web service is still on beta version, consume it at your own risks:
   * x: x coordinate value (longitude) in decimal degrees or meters.
   * y: y coordinate value (latitude) in decimal degrees or meters.
   * in: input system code. The code must be registered in TWCC database. Ex.: "WGS84".
-  * out: output system code. The code must be registered in TWCC database. Ex.: "EPSG:4230".
+  * out: output system code. The code must be registered in TWCC database. Ex.: "EPSG:4230" (remember to URL encode it).
         
- - Example of url: http://twcc.fr/en/ws/?fmt=json&x=33&y=44&in=WGS84&out=EPSG:4230
+ - Example of url: [http://twcc.fr/en/ws/?fmt=json&x=33&y=44&in=WGS84&out=EPSG%3A4230](http://twcc.fr/en/ws/?fmt=json&x=33&y=44&in=WGS84&out=EPSG%3A4230)
     
-## Deploy process
+## Deploy process:
+
+### Web application:
 
 In the project's root run the following:
-```
+```shell script
 $ npm install
 $ cd webapp
 $ npm install
 $ npm run build
 $ cd ..
 $ grunt -f
+```
+
+### Web service:
+
+In the project's root run the following:
+```shell script
+$ cd webapp/includes
+$ php composer.phar install
 ```
--- webapp/ws/index.php
+++ webapp/ws/index.php
@@ -15,24 +15,25 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with TWCC.  If not, see <http://www.gnu.org/licenses/>.
  *
- * @copyright Copyright (c) 2010-2014 Clément Ronzon
+ * @copyright Copyright (c) 2010-2014 Clï¿½ment Ronzon
  * @license http://www.gnu.org/licenses/agpl.txt
  */
-define('LIB_PATH', substr(dirname(__FILE__), 0, -2).'includes/lib/');
+define('INCLUDES_PATH', substr(dirname(__FILE__), 0, -2).'includes/');
+define('VENDOR_PATH', INCLUDES_PATH.'vendor/');
+
+require(INCLUDES_PATH.'application_top.php');
 
 define('NO_ERROR_CODE', -2);
 define('MISSING_PARAMETER_ERROR_CODE', 2);
 define('WRONG_FORMAT_ERROR_CODE', 3);
-define('DEFINITION_ERROR_CODE', 4);
-define('UNEXPECTED_ERROR_CODE', 5);
-define('PROJ4PHP_ERROR_CODE', 6);
+define('UNEXPECTED_ERROR_CODE', 4);
+define('PROJ4PHP_ERROR_CODE', 5);
 
 $error_msgs = Array(
   2=> "Some parameters are missing. Please read this WS documentation.",
   3=> "Wrong value for format parameter. Possible values are: xml, json.",
-  4=> "Issue during CRS definition intialization. Please contact us.",
-  5=> "Unexpected error. Please contact us.",
-  6=> "Core error. Please contact us. Message: "
+  4=> "Unexpected error. Please contact us.",
+  5=> "Core error. Please contact us. Message: "
 );
 
 define('LATITUDE_PARAM_LABEL', 'y');
@@ -49,14 +50,14 @@ define('DEFAULT_FORMAT', 'xml');
 
 function printErrorResponse($format, $error, $message) {
   if ($format=='json') {
-    echo "{\"status\":\"error\", \"erreur\":{\"code\":" . $error . ", \"message\":\"" . $message . "\"}}";
+    echo "{\"status\":\"error\", \"error\":{\"code\":" . $error . ", \"message\":\"" . $message . "\"}}";
     exit;
   } else {
     echo "<reponse>";
-      echo "<erreur>";
+      echo "<error>";
         echo "<code>" . $error . "</code>";
         echo "<message>" . $message . "</message>";
-      echo "</erreur>";
+      echo "</error>";
     echo "</reponse>";
     exit;
   }
@@ -66,8 +67,8 @@ $error = NO_ERROR_CODE;
 
 $x = isset($_GET[LONGITUDE_PARAM_LABEL]) ? $_GET[LONGITUDE_PARAM_LABEL] : DEFAULT_LONGITUDE;
 $y = isset($_GET[LATITUDE_PARAM_LABEL]) ? $_GET[LATITUDE_PARAM_LABEL] : DEFAUlT_LATITUDE;
-$projectionxy = isset($_GET[INPUT_CRS_PARAM_LABEL]) ? str_replace('::',':',$_GET[INPUT_CRS_PARAM_LABEL]) : DEFAULT_INPUT_CRS;
-$projection = isset($_GET[OUTPUT_CRS_PARAM_LABEL]) ? str_replace('::',':',$_GET[OUTPUT_CRS_PARAM_LABEL]) : DEFAULT_OUTPUT_CRS;
+$projectionxy = isset($_GET[INPUT_CRS_PARAM_LABEL]) ? cleanString(str_replace('::',':',$_GET[INPUT_CRS_PARAM_LABEL])) : DEFAULT_INPUT_CRS;
+$projection = isset($_GET[OUTPUT_CRS_PARAM_LABEL]) ? cleanString(str_replace('::',':',$_GET[OUTPUT_CRS_PARAM_LABEL])) : DEFAULT_OUTPUT_CRS;
 $format = isset($_GET[FORMAT_PARAM_LABEL]) ? $_GET[FORMAT_PARAM_LABEL] : DEFAULT_FORMAT;
 
 // check the parameters
@@ -81,22 +82,25 @@ if ($format=='json') {
   header('Content-type: text/xml');
 }
 
+include(VENDOR_PATH."autoload.php");
+
+use proj4php\Proj4php;
+use proj4php\Proj;
+use proj4php\Point;
+
 try {
-  include_once(LIB_PATH.'proj4php.php');
-
   $proj4 = new Proj4php();
-  $projsource = new Proj4phpProj($projectionxy,$proj4);
-  $projdest = new Proj4phpProj($projection,$proj4);
 
-  // check the projections
-  $error = (Proj4php::$defs[$projectionxy]==Proj4php::$defs['WGS84'] && $projectionxy!='WGS84') ? DEFINITION_ERROR_CODE : $error;
-  $error = (Proj4php::$defs[$projection]==Proj4php::$defs['WGS84'] && $projection!='WGS84') ? DEFINITION_ERROR_CODE : $error;
-
-  if ($error != NO_ERROR_CODE) {
-    printErrorResponse($format, $error, $error_msgs[$error]);
+  $sql = "SELECT Code, Definition FROM coordinate_systems WHERE Code IN ('$projectionxy', '$projection')";
+  $crs_query = tep_db_query($sql);
+  while ($crs = tep_db_fetch_array($crs_query)) {
+    $proj4->addDef($crs['Code'],$crs['Definition']);
   }
 
-  $pointSrc = new proj4phpPoint($x, $y);
+  $projsource = new Proj($projectionxy,$proj4);
+  $projdest = new Proj($projection,$proj4);
+
+  $pointSrc = new Point($x, $y);
 
   $pointDest = $proj4->transform($projsource, $projdest, $pointSrc);
 } catch (Exception $e) {
